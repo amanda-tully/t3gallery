dd911e0b745c884ec7458d65fef62c04b0125296
Amanda Tully <amandadianacmd@gmail.com>
2025-04-14 11:41:53 +0200

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoILvKWqNMTGruRNDbf86ZaKhFy0tC7LOX3YWs",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoCzevJKEmO527k0FwYhx6fDeTIoZ8EuvqipAs",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoS8n2foex9cPCQVrXyzDnfTAGmO2BHUWhajKF",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoo9axMMrMwnYE0xs4Vl9TzQvaDuejm2JrLNHX",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoXWUZh0GvlToI7ajS3F1Wg5ympwEM0nBNiKfV",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoQc5zCuTq8Pn1hXCpQ7WExiGB23guU4cOwT6a",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoHWUWQfd4N6v8r0sPDZSOXEiM7mYcobu1qjxz",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVovQjR8GAGqkTOPo9RXCiJbtfZ6A3KQvaIjygY",
+  "https://qyieefd79u.ufs.sh/f/fcF0jp2zvrVoHWyvigI4N6v8r0sPDZSOXEiM7mYcobu1qjxz",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress
+    <main>
+      <div className="flex flex-wrap gap-2">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + index} className="w-48 p-4">
+            <img src={image.url} />
+          </div>
+        ))}
+        Hello gallery in progress
+      </div>
     </main>
   );
 }
